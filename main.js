9c7dac915b00697601eed282ffee194a0aa1abc7
Daniel Dexter M <migunok12@gmail.com>
2024-07-24 15:31:29 +0300

    Made popup form.

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+function popUpForm(){
+  const overlay = document.getElementById('popUpContainer');
+  overlay.classList.toggle('show');
+}
+
+
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.sayInfo = function() {
+      console.log(this.name, this.author, this.pages, this.read);
+    }
 }
 
 function addBookToLibrary() {
