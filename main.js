d31116c9badec4871f1da7c21654d7748a4c3418
Daniel Dexter M <migunok12@gmail.com>
2024-10-05 22:26:52 +0300

    Added functionality (Add books) and UI improvement

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 const overlay = document.getElementById('popUpContainer');
 const closeButton = document.getElementById('popUpCloseButton')
-let forms = document.querySelectorAll('input');
+const Library = document.getElementById('library');
 
 // Open and close overlay to add book
 
@@ -13,26 +12,73 @@ function popUpForm(){
 function closeForm(){
   overlay.classList.remove('show');
   overlay.classList.add('hide');
+  clearForm();
 }
 
 // Clear all form input
 
-closeButton.addEventListener('click', () => {
+function clearForm(){
+  const forms = document.querySelectorAll('.form-input');
   forms.forEach(form => form.value = '');
-})
+
+  const checkBox = document.querySelector('.form-checkbox')
+  checkBox.checked = false
+}
+
+// Toggle READ/UNREAD
+
+function toggleRead(){
+
+}
+
+// Remove book from Display
+
+function removeBook(){
+
+}
 
 // Content
 
 const myLibrary = [];
 
-function Book(name, author, pages, read) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book() {
+    Library.innerHTML = '';
+
+    myLibrary.forEach(bookinfo => {
+
+      const bookDiv = document.createElement('div');
+      bookDiv.className = 'bookDiv';
+    
+      bookDiv.innerHTML = `
+          <h3 id="bookName">"${bookinfo.name}"</h3>
+          <p id="bookAuthor"> ${bookinfo.author}</p>
+          <p id="bookPages">${bookinfo.pages} pages</p>
+          <button class="bookDisplayButton" id="readButton" onClick="toggleRead()"> ${bookinfo.read} </button>
+          <button class="bookDisplayButton" id="removeButton" onClick="removeBook()">Remove</button>
+      `;
+    
+      Library.appendChild(bookDiv);
+    });
+
+
 }
 
-
 function addBookToLibrary() {
-  // do stuff here
+
+  const name = document.getElementById('name').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read').checked;
+
+    bookInfo = {
+      name : name,
+      author : author,
+      pages : pages,
+      read : read
+    };
+
+    myLibrary.push(bookInfo)
+    closeForm()
+    Book()
+    
 }
